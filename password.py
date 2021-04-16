c1b64a57ec3320660a11f4adf420b73f4fa24c68
markser <serm@oregonstate.edu>
2021-04-15 17:21:22 -0700

    working, but only getting from the first part of the chars string

--- password.py
+++ password.py
@@ -5,3 +5,12 @@ NUMBERS = string.digits
 PUNCTUATION = string.punctuation  
 
 def passwordGenerator(n):
+    chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
+    password = ''
+    for x in range(0,n):
+        randomNumber = random.randint(0,n)
+        password += chars[randomNumber]
+    print(password)
+
+
+passwordGenerator(10)
